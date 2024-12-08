919531389439ac94277106f828bd190f08024517
phg02 <s4063236@rmit.edu.vn>
2024-12-08 21:07:11 +0700

    setup

---
+++ app.py
@@ -0,0 +1,10 @@
+from flask import Flask, render_template, url_for
+
+app = Flask(__name__)
+
+@app.route('/')
+def hello_world():
+    return render_template('signup.html');
+
+if __name__ == '__main__':
+    app.run(debug=True)
